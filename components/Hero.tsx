dffc95fdbec1597612d7317017af4df581b361b3
mdyhakash <mdyhakash@gmail.com>
2025-01-24 20:12:06 +0600

    Full Portfolio Done

---
+++ components/Hero.tsx
@@ -0,0 +1,54 @@
+import { IoMdDownload } from "react-icons/io";
+
+import MagicButton from "./ui/MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89px] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          {/* <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </h2> */}
+
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-4xl lg:text-5xl"
+            words="Bringing ideas to life with modern web technologies and pixel-perfect precision."
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Akash, a Frontend Web developer based in Bangladesh.
+          </p>
+
+          <a href="/resume.pdf" download="Md_Yasin_Hossain_Akash_Resume.pdf">
+            <MagicButton
+              title="Download Resume"
+              icon={<IoMdDownload />}
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ data/index.ts
@@ -0,0 +1,202 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  //   {
+  //     id: 1,
+  //     title: "I prioritize client collaboration, fostering open communication ",
+  //     description: "",
+  //     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+  //     imgClassName: "w-full h-full",
+  //     titleClassName: "justify-end",
+  //     img: "/b1.svg",
+  //     spareImg: "",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "I'm very flexible with time zone communications",
+  //     description: "",
+  //     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+  //     imgClassName: "",
+  //     titleClassName: "justify-start",
+  //     img: "",
+  //     spareImg: "",
+  //   },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently working on a project NeoED",
+    description: "",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "ShasthyaSetu",
+    des: "A web-based platform designed to provide users with easy access to healthcare services. Built during the Dhaka Divisional Hackathon.",
+    img: "/shasthyasetu.png",
+    link: "https://shasthyasetu.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Adibas - An E-commerce Web App",
+    des: "An e-commerce platform designed for seamless shopping experiences, featuring dynamic product listings, a cart system, and secure checkout.",
+    img: "/adibas.png",
+    link: "https://e-commerce-mdyhakash.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Real Estate Agency App",
+    des: "A web application enabling users to browse, filter, and book properties seamlessly, with dynamic property listings and user-friendly design.",
+    img: "/realestate.png",
+    link: "https://real-state-agency-mdyhakash.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Eutropia IT Portfolio Frontend",
+    des: "Developed the frontend of a corporate portfolio website for Eutropia IT, showcasing their services and projects with a responsive and modern design.",
+    img: "/eutropia.png",
+    link: "https://game-listing-one.vercel.app/",
+  },
+];
+
+// export const testimonials = [
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+// ];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Web Developer",
+    desc: "Intern at Eutropia IT - Contributed to building engaging and responsive web interfaces.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 2,
+    title: "Hackathon Participant",
+    desc: "Participated in 2 national hackathons, showcasing problem-solving and teamwork skills.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/mdyhakash",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/mdyhakash",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/mdyhakash/",
+  },
+];
